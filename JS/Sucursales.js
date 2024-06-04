49572d136cc233c539ab34ae0a844965484ac5a1
M-Monzon <fernandezmonzonmiguel@gmail.com>
2024-06-03 22:30:51 -0300

    Datos3

--- JS/Sucursales.js
+++ JS/Sucursales.js
@@ -1,3 +1,13 @@
+let original = document.querySelector("#Sucursales");
+let contenedor = document.querySelector("#Contenedor"); 
+
+let botonsuc1 = document.querySelector("#suc1");
+let botonsuc2 = document.querySelector("#suc2");
+let botonsuc3 = document.querySelector("#suc3");
+
+let referencia = original.cloneNode(true);
+original.remove() // Clono y elimino el original
+
 //Consultando al JSON propio
 
 fetch("https://M-Monzon.github.io/CAC_Proyecto/datos.json")
@@ -6,12 +16,16 @@ fetch("https://M-Monzon.github.io/CAC_Proyecto/datos.json")
     // Procesamiento de la info que llega de la API
     console.log(data);
 
-    console.log(data.Sucursal1[0])
-    console.log(img1)
+    console.log("Acercate a " + data.Sucursal1[1] + " en el famoso Barrio de " + data.Sucursal1[0]) //Direccion
+    console.log(data.img1) //Imagen
 
+    let Sucursal1 = referencia.cloneNode(true);
 
+    sucursal1.querySelector("img").src = data.img1;
+    sucursal1.querySelector("img").alt = "Foto de la sucursal";
+    sucursal1.querySelector("p").innerHTML = "Acercate a " + data.Sucursal1[1] + " en el famoso Barrio de " + data.Sucursal1[0];
 
-
+    contenedor.appendChild(Sucursal1); 
     })
 .catch(error => console.log("Ocurrió un error!" + error));
 
