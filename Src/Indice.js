2d088a7d38d08dd23415e05301bfad7d78830d9c
M-Monzon <fernandezmonzonmiguel@gmail.com>
2024-06-03 21:00:06 -0300

    Datos

---
+++ Src/Indice.js
@@ -0,0 +1,72 @@
+console.log("Hola mundo") // Imprimir en consola
+
+/* 
+VARIABLES
+
+    1 - let/var/(nombre de variable) Creo una variable
+    2 - Menciona los tipos de datos.
+    3 - Constantes: un valor que no cambia. Ej: const IVA = 1.21; (la constante la pongo en mayuscula)
+
+TIPOS DE DATOS
+
+    1 - Tipo de datos (number, string, boolean)
+        number: Para JS int y float son lo mismo.
+    2 - Como ver en la consola el tipo de dato.
+
+OPERADORES ARITMETICOS (Tips, meter cosas entre parentesis) 
+
+ALERTAS (Mensajes emergentes)
+ALERTAS DE CONFIRMACION (Mensajes emergentes con confirmacion)
+PROMPT (Mensaje emergente con ingreso de datos)
+
+    1 - Casteo de datos de cadena a numero.
+
+OPERADORES LOGICOS
+
+ESTRUCTURAS DE CONTROL 
+
+    1 - If/else
+    2 - If ternario (Para cosas sencillas)
+    3 - Switch (Casos, default)
+    4 - While (Mientras)
+    5 - For: (valor inicial; condicion; incremento)
+    6  While/IF continuar. Buen ejemplo de pedir un valor y continuar cuando se cumple
+    7 - Do while: Hace algo y despues pregunta si lo hace de nuevo
+    8 - Funciones 
+        Ejemplo con el boton
+        Ejemplo con el return
+        Funciones flecha (pensada para funciones pequeñas y rapidas. Son aninomas)
+            x => x + 1
+
+SCOPE
+
+PPO
+        
+        1 - Clases
+        2 - Metodos (funciones dentro de una clase)
+ 
+ARRAYS (Listas)
+
+JSON (Objetos)
+    1 - Es una forma de notar objetos. Diccionarios clave/valor (53)
+    2 - Se pueden guardar strins, numeros, booleanos, arrays, objetos.
+    3 - JSON.stringify() y JSON.parse(57) 
+    4 - Como se consume una API (1,8 aprox)
+        fetch
+
+    5 - Consumir una api desde GitHub (1.22 aprox)
+
+DOM
+
+     
+
+CONSIGNAS DEL TP (Actualizadas)
+
+    Hacer un fetch a una API
+    Mostrarla en la pagina
+
+
+
+*/
+
+// Comentario de linea
